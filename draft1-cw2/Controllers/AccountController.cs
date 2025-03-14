a6a4c28165b7f7ef785ecd0fed7730cd0095da70
TIBERIU-SEPTIMIU UNGURIANU <TIU0010@my.londonmet.ac.uk>
2025-03-14 16:30:27 +0000

    Add project files.

---
+++ draft1-cw2/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YourMvcApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /Account/Login
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: /Account/Login
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            // Replace this with your actual authentication logic.
+            if (username == "admin" && password == "password")
+            {
+                // On successful login, redirect to Home page.
+                return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError("", "Invalid login attempt.");
+            return View();
+        }
+    }
+}
